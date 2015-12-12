414e2c1f08d11e3ac22cf55ef902264e3e78af06
Bart Dietl <bart@tuta.io>
2015-12-12 16:51:53 +0100

    Created UI

--- Kill the Monster/ViewController.swift
+++ Kill the Monster/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var playerAtkLbl: UILabel!
+    @IBOutlet weak var enemyAtkLbl: UILabel!
+    @IBOutlet weak var infoLbl: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onPlayerButtonPressed(sender: AnyObject) {
+    }
 
+    @IBAction func onEnemyButtonPressed(sender: AnyObject) {
+    }
 }
 
